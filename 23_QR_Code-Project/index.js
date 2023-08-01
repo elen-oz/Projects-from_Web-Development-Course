fba3b5c4ec12a5b313a88d16190bbb78f2e4b800
Elena Ozerova <ozerova.lenka@gmail.com>
2023-08-01 14:23:42 +0200

    finish QR-generetor img project

--- 23_QR_Code-Project/index.js
+++ 23_QR_Code-Project/index.js
@@ -1,7 +1,7 @@
 import inquirer from 'inquirer';
-// import qr-image from
+import qr from 'qr-image';
+import fs from 'fs';
 
-// 1. Use the inquirer npm package to get user input.
 inquirer
   .prompt([
     {
@@ -10,8 +10,14 @@ inquirer
     },
   ])
   .then((answers) => {
-    console.log(answers);
     const url = answers.URL;
+    var qr_svg = qr.image(url);
+    qr_svg.pipe(fs.createWriteStream('qr_img.png'));
+
+    fs.writeFile('URL.txt', url, (err) => {
+      if (err) throw err;
+      console.log('Thr file has been saved!');
+    });
   })
   .catch((error) => {
     if (error.isTtyError) {
@@ -20,9 +26,3 @@ inquirer
       // Something else went wrong
     }
   });
-
-/* 
-
-2. Use the qr-image npm package to turn the user entered URL into a QR code image.
-3. Create a txt file to save the user input using the native fs node module.
-*/
